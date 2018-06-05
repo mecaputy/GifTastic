21aa4c377b852c15732f04c255f490de82084ff7
mecaputy <martina.e.caputy@gmail.com>
2018-06-04 22:20:50 -0400

    edits

--- assets/javascript/main.js
+++ assets/javascript/main.js
@@ -1,21 +1,21 @@
 var animals = ["dog", "cat", "rabbit", "hamster", "skunk"];
 
 function display() {
-    
-    var userInput = $(this).attr("data-name");
+
+    var userInput = $(this).attr("value");
     var queryURL = "http://api.giphy.com/v1/gifs/search?q=" +
-    userInput + "&api_key=YD0szooQbzKyuftqi9OoQZwr8r4YItjy&limit=10";
-    
+        userInput + "&api_key=YD0szooQbzKyuftqi9OoQZwr8r4YItjy&limit=10";
+
 
     //create an AJAX call for button being clicked
     $.ajax({
         url: queryURL,
         method: "GET"
-    }).then(function(response) {
+    }).then(function (response) {
 
         $("#gifs-view").html("");
 
-        for (var i = 0; i < animals.length; i++) {
+        for (var i = 0; i < response.data.length; i++) {
             //log the response from API
             console.log(response);
             //still image URL
@@ -26,7 +26,7 @@ function display() {
             console.log(response.data[i].rating);
             //display gif
 
-            var gifImage = $("<img class='imgAnimate'>");
+            var gifImage = $("<img>").addClass("imgAnimate");
             //initial image to show as still
             gifImage.attr("src", response.data[i].images.fixed_height_still.url);
             //animated path
@@ -37,8 +37,8 @@ function display() {
             //add animate funtion on click
             gifImage.on("click", playGif);
 
-            var displayDiv = $("<div class='container-div'>");
-            var ratingDiv = $("<div class='rating-div'");
+            var displayDiv = $("<div>").addClass("container-div");
+            var ratingDiv = $("<div>").addClass("rating-div");
 
             ratingDiv.text("Rating: " + response.data[i].rating);
 
@@ -53,51 +53,48 @@ function display() {
     });
 
 };
-//function to render buttons when the submit button is clicked
-$("#add-animal").on('click', function renderButtons() {
 
-    var userInput = $("#animal-input").val();
-
-    if (userInput === "") {
-        // alert the user of invalid response
-        alert("You must enter an animal")
-
-    } else {
-        //create new button from input
-        var newBtn = $("<button>");
-        newBtn.attr("value", userInput);
-        newBtn.text(userInput);
-        //add new button to existing buttons
-        $("#buttons-view").append(newBtn);
-
-        //clear the input box
-        $("#animal-input").val("");
-    }
-
-});
-
-$("#add-animal").on("click", function(event) {
-    event.preventDefault();
-
-    var userInput = $("#animal-input").val().trim();
-
-    animals.push(userInput);
-
-    renderButtons();
-  });
-
-
-function playGif(){
+function playGif() {
     var state = $(this).attr("data-state");
-			    
-			if (state == "still") {
-				$(this).attr("src", $(this).data("animate"));
-				$(this).attr("data-state", "animate");
-			} else {
-				$(this).attr("src", $(this).data("still"));
-				$(this).attr("data-state", "still");
-			}
+
+    if (state == "still") {
+        $(this).attr("src", $(this).data("animate"));
+        $(this).attr("data-state", "animate");
+    } else {
+        $(this).attr("src", $(this).data("still"));
+        $(this).attr("data-state", "still");
+    }
 }
 
 $(document).on("click", ".animal-btn", display);
 
+
+$(document).ready(function () {
+    //function to render buttons when the submit button is clicked
+    $("#add-animal").on('click', function (e) {
+        e.preventDefault();
+
+        var userInput = $("#animal-input").val().trim();
+
+        if (userInput === "") {
+            // alert the user of invalid response
+            alert("You must enter an animal")
+
+        } else {
+            //if the button doesn't already exists
+            if (animals.indexOf(userInput) == -1) {
+                animals.push(userInput);
+                //create new button from input
+                var newBtn = $("<button>");
+                newBtn.attr("value", userInput);
+                newBtn.addClass('animal-btn');
+                newBtn.text(userInput);
+                //add new button to existing buttons
+                $("#animal-btns").append(newBtn);
+            }
+            //clear the input box
+            $("#animal-input").val("");
+        }
+
+    });
+});
